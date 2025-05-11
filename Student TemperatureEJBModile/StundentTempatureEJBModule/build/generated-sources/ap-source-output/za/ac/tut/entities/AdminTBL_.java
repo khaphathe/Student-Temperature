d7c56d733960627d441beb17ab0368bddb8af65c
LebogangL <letau202@gmail.com>
2025-05-11 17:29:22 +0200

    Add files via upload
    @ENHLE23

---
+++ Student TemperatureEJBModile/StundentTempatureEJBModule/build/generated-sources/ap-source-output/za/ac/tut/entities/AdminTBL_.java
@@ -0,0 +1,18 @@
+package za.ac.tut.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-09T11:02:31")
+@StaticMetamodel(AdminTBL.class)
+public class AdminTBL_ { 
+
+    public static volatile SingularAttribute<AdminTBL, String> password;
+    public static volatile SingularAttribute<AdminTBL, String> surname;
+    public static volatile SingularAttribute<AdminTBL, String> contact;
+    public static volatile SingularAttribute<AdminTBL, String> name;
+    public static volatile SingularAttribute<AdminTBL, Long> id;
+    public static volatile SingularAttribute<AdminTBL, String> email;
+
+}
---
+++ Student TemperatureEJBModile/StundentTempatureEJBModule/build/generated-sources/ap-source-output/za/ac/tut/entities/StudentsTBL_.java
@@ -0,0 +1,19 @@
+package za.ac.tut.entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-09T11:02:31")
+@StaticMetamodel(StudentsTBL.class)
+public class StudentsTBL_ { 
+
+    public static volatile SingularAttribute<StudentsTBL, String> gender;
+    public static volatile SingularAttribute<StudentsTBL, String> surname;
+    public static volatile SingularAttribute<StudentsTBL, String> name;
+    public static volatile SingularAttribute<StudentsTBL, Double> temperature;
+    public static volatile SingularAttribute<StudentsTBL, Long> id;
+    public static volatile SingularAttribute<StudentsTBL, Date> creationDate;
+
+}
